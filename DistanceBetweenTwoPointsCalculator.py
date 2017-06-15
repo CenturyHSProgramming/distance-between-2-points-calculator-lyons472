0cc8ef125908fa42218f47cab33d217e45e2beee
Lyons_Stephen <lyons472@hsd.k12.or.us>
2017-06-15 09:23:16 -0700

    Job is done

--- DistanceBetweenTwoPointsCalculator.py
+++ DistanceBetweenTwoPointsCalculator.py
@@ -8,7 +8,19 @@
 # Define Function below
 # be sure to return an integer
 
+import math
+#Distance = (((xB-xA)**2)+((yB-yA)**2)**(0.5))
+#where A is the first point, B is the second point, and x and y are their values.
+def calculateDistanceBetween (xA, yA, xB, yB):
+
+    Distance = (((xB-xA)**2)+((yB-yA)**2))
+    Distance = math.sqrt((((xB-xA)**2)+((yB-yA)**2)))
+    Distance = round(Distance, 2)
+    return Distance
+    
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
     pass # remove or comment out this line if you wish to test the function
+    answer = calculateDistanceBetween(2,4,26,9)
+    print(answer)
